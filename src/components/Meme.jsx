b2075fd8085c483aabbb2e352899e951fbfffc5f
andrea ilieschi <andrea.ilieschi@gmail.com>
2024-03-08 18:56:30 +0100

    edit css

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,49 @@
-import "./meme.css"
+import { useState } from 'react';
+import './meme.css';
+
+import memesData from '../assets/memesData.js';
 
 export default function Meme() {
-    return (
-        <main>
-            <form className="form">
-                <input type="text" placeholder="Top text" className="form--input" />
-                <input type="text" placeholder="Bottom text" className="form--input" />
-                <button className="form--button">Generate Meme</button>
-            </form>
-        </main>
+  const [memeImage, setMemeImage] = useState(memesData.data.memes[5].url);
+
+  function generateMeme() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(
+       memesArray[randomNumber].url
     )
+
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <label htmlFor="top-text">
+          Top text
+          <input
+            id="top-text"
+            type="text"
+            placeholder="Top text here"
+            className="form--input"
+            name="toptext"
+          />
+        </label>
+        <label htmlFor="bottom-text">
+          Bottom text
+          <input
+            id="bottom-text"
+            type="text"
+            placeholder="Bottom text here"
+            className="form--input"
+          />
+        </label>
+        <button onClick={generateMeme} className="form--button">
+          Generate Meme ⚙️
+        </button>
+      </div>
+     
+        <img className="meme-image" src={memeImage} alt="meme img" />
+ 
+    </main>
+  );
 }
